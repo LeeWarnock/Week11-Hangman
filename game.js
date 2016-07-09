65fce0bd0e63398e8360558dda3b41c7f189b472
Lee Warnock <clwarnock@gmail.com>
2016-07-09 17:27:49 -0500

    committing all 4 files before npm init

--- game.js
+++ game.js
@@ -1,4 +1,9 @@
 exports.game = {
 	//this.wordChoices = ["heart and soul", "relax", "burning up", "manic monday", "into the groove", "invisible touch", "rebel yell"];
-	wordBank : ["relax"]
+	wordBank : ["monitor", "program", "application", "keyboard", "javascript",
+	"gaming", "network", "geography", "cat", "yesterday", "java", "truck", "opportunity",
+	"fish", "token", "transportation", "bottom", "apple", "cake",
+	"remote", "pocket", "terminology", "arm", "cranberry", "tool",
+	"caterpillar", "spoon", "watermelon", "laptop", "toe", "toad",
+	"fundamental", "capitol", "garbage", "anticipate", "apple", "penispenispenis"],
 };
